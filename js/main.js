6675755fb91106e914ba26b519bb7772c5d0d8bc
Gianluca Costa <gianluca@gianlucacosta.info>
2016-11-22 01:12:53 +0100

    Introduce website

---
+++ js/main.js
@@ -0,0 +1,99 @@
+var gameBaseURL =
+  "https://github.com/giancosta86/TwoBinGame/releases/latest"
+
+var managerBaseURL =
+  "https://github.com/giancosta86/TwoBinManager/releases/latest"
+
+
+function onMobileDevice() {
+  return window.screen.availWidth < 840
+}
+
+
+window.onload = function() {
+  if (!onMobileDevice()) {
+    setupRunWithMoonDeployButtons()
+    setupDownloadBinaryZipButtons()
+  }
+}
+
+
+function setupRunWithMoonDeployButtons() {
+  getLatestMoonDescriptor(
+    gameBaseURL,
+
+    "App.moondeploy",
+
+    function(descriptorURL) {
+      var runButton = document.getElementById("runGame")
+      runButton.href = descriptorURL
+      runButton.style.display = "inline-block"
+    },
+
+    function(statusCode) {
+      console.log("Error while retrieving the MoonDeploy descriptor for TwoBinGame")
+      console.log(statusCode)
+    }
+  )
+
+
+  getLatestMoonDescriptor(
+    managerBaseURL,
+
+    "App.moondeploy",
+
+    function(descriptorURL) {
+      var runButton = document.getElementById("runManager")
+      runButton.href = descriptorURL
+      runButton.style.display = "inline-block"
+    },
+
+    function(statusCode) {
+      console.log("Error while retrieving the MoonDeploy descriptor for TwoBinManager")
+      console.log(statusCode)
+    }
+  )
+}
+
+
+
+function setupDownloadBinaryZipButtons() {
+  getLatestReleaseArtifact(
+    gameBaseURL,
+
+    "TwoBinGame",
+
+    ".zip",
+
+    function(zipURL) {
+      var downloadBinaryZipButton = document.getElementById("downloadGame")
+      downloadBinaryZipButton.href = zipURL
+      downloadBinaryZipButton.style.display = "inline-block"
+    },
+
+    function(statusCode) {
+      console.log("Error while retrieving the binary zip artifact for TwoBinGame")
+      console.log(statusCode)
+    }
+  )
+
+
+  getLatestReleaseArtifact(
+    managerBaseURL,
+
+    "TwoBinManager",
+
+    ".zip",
+
+    function(zipURL) {
+      var downloadBinaryZipButton = document.getElementById("downloadManager")
+      downloadBinaryZipButton.href = zipURL
+      downloadBinaryZipButton.style.display = "inline-block"
+    },
+
+    function(statusCode) {
+      console.log("Error while retrieving the binary zip artifact for TwoBinManager")
+      console.log(statusCode)
+    }
+  )
+}
